e36576409aa5f80fa818491e403f3b483bba3946
kimminse3380 <85288381+kimminse3380@users.noreply.github.com>
2022-07-21 20:48:25 +0900

    LED_BLINK

---
+++ MyADC/Core/Src/__stdio.c
@@ -0,0 +1,31 @@
+/*
+ * __stdio.c
+ *
+ *  Created on: Jun 28, 2022
+ *      Author: mss33
+ */
+
+#include "main.h"
+
+extern  UART_HandleTypeDef huart2;
+
+int __io_putchar(int ch)
+{
+	if (ch == '\n') {
+		HAL_UART_Transmit(&huart2, (uint8_t *)"\r", 1, 0xFFFF);
+	}
+	HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+
+	return ch;
+}
+
+int __io_getchar(void)
+{
+	uint8_t ch = 0;
+
+	__HAL_UART_CLEAR_OREFLAG(&huart2);
+
+	HAL_UART_Receive(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+
+	return ch;
+}
